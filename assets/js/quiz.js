e7cba5e2cc0fbd8c6ad812672a49cfb7546c9d69
daisygunn <daisy.l.g@hotmail.co.uk>
2021-07-15 19:57:54 +0000

    Remove unnessecary files from planetary library

---
+++ assets/js/quiz.js
@@ -0,0 +1,31 @@
+// const countries = [{
+//         name: 'England',
+//         capital: 'London',
+//     },
+//     {
+//         name: 'Scotland',
+//         capital: 'Edinburgh'
+//     },
+//     {
+//         name: 'Spain',
+//         capital: 'Madrid'
+//     },
+// ];
+
+// let quiz = [];
+
+// const quizExample = [{
+//     question: 'What is the capital of England?',
+//     answers: [{
+//             answer: 'London',
+//             isCorrect: true
+//         },
+//         {
+//             answer: 'Dublin',
+//             isCorrect: false
+//         }
+//     ]
+
+// }];
+
+// quiz.push({})
