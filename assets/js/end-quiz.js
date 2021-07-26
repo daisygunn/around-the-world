4e7ee511ba93c8920e3bc2707aea4a196d01babc
daisygunn <daisy.l.g@hotmail.co.uk>
2021-07-26 10:36:09 +0000

    Add function to end-quiz.js to make Game Over heading blink

---
+++ assets/js/end-quiz.js
@@ -0,0 +1,11 @@
+// Blinking Game Over heading - https://www.youtube.com/watch?v=9iDm3QiIEzU
+
+setInterval(function(){
+    var gameOverText = document.getElementById('game-over-heading');
+
+    gameOverText.innerHTML="Game Over!";
+    
+    gameOverText.style.display=(txt.style.display=='none'?'':'none');
+}, 600);
+
+
