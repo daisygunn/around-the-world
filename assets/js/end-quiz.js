1bb972b2742ff497312b2e59476a3b392d9e3f64
daisygunn <daisy.l.g@hotmail.co.uk>
2021-07-29 13:18:12 +0000

    Add js to end-quiz.js to save high scores to local storage and display them on high-scores.html

--- assets/js/end-quiz.js
+++ assets/js/end-quiz.js
@@ -1,5 +1,4 @@
 // Blinking Game Over heading - https://www.youtube.com/watch?v=9iDm3QiIEzU
-
 setInterval(function(){
     var gameOverText = document.getElementById('game-over-heading');
 
@@ -9,3 +8,36 @@ setInterval(function(){
 }, 600);
 
 
+const username = document.getElementById('username');
+const saveScore = document.getElementById('save-score');
+const finalScore = document.getElementById('final-score-total');
+const mostRecentScore = localStorage.getItem('recentScore');
+const highScores = JSON.parse(localStorage.getItem('highScores'));
+
+const maxHighScores = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScore.disabled = !username.value
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+
+const score = {
+    score: mostRecentScore,
+    name: username
+}
+
+highScores.push(score)
+
+highScores.sort((a,b) => {
+    return b.score - a.score
+})
+
+highScores.splice(5)
+
+localStorage.setItem('highScores', JSON.stringify(highScores))
+window.location.assign('/')
+}
