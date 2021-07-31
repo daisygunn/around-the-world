c87b58b6d52b404a916a6364c70fc7a3bf3e0cda
daisygunn <daisy.l.g@hotmail.co.uk>
2021-07-31 08:25:20 +0000

    Reenable blinking game over heading function

--- assets/js/end-quiz.js
+++ assets/js/end-quiz.js
@@ -1,11 +1,11 @@
-// // Blinking Game Over heading - https://www.youtube.com/watch?v=9iDm3QiIEzU
-// setInterval(function () {
-//     var gameOverText = document.getElementById('game-over-heading');
+// Blinking Game Over heading - https://www.youtube.com/watch?v=9iDm3QiIEzU
+setInterval(function () {
+    var gameOverText = document.getElementById('game-over-heading');
 
-//     gameOverText.innerHTML = "Game Over!";
+    gameOverText.innerHTML = "Game Over!";
 
-//     gameOverText.style.display = (gameOverText.style.display == 'none' ? '' : 'none');
-// }, 600);
+    gameOverText.style.display = (gameOverText.style.display == 'none' ? '' : 'none');
+}, 600);
 
 
 const username = document.getElementById('username');
