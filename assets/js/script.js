4dbd6872db2fe7740659d5111e489efb42e1452b
daisygunn <daisy.l.g@hotmail.co.uk>
2021-07-31 16:35:26 +0000

    Add css and js code for responsive navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@
 let textWrapper = document.querySelector('.animated-heading .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
+anime.timeline({
+    loop: true
+  })
   .add({
     targets: '.animated-heading .letter',
     translateY: ["1.1em", 0],
@@ -18,4 +20,23 @@ anime.timeline({loop: true})
     delay: 1000
   });
 
-  
+// Hamburger menu code found here https://dev.to/devggaurav/let-s-build-a-responsive-navbar-and-hamburger-menu-using-html-css-and-javascript-4gci
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+};
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+};
