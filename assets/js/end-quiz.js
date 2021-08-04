78637fb88d695d6cfaab263384e3412a7dfcf8e5
daisygunn <daisy.l.g@hotmail.co.uk>
2021-08-04 15:12:49 +0000

    Add explanatory comments to end-quiz.js, add position rules to end-page-footer

--- assets/js/end-quiz.js
+++ assets/js/end-quiz.js
@@ -9,37 +9,46 @@ setInterval(function () {
     gameOverText.style.display = (gameOverText.style.display == 'none' ? '' : 'none');
 }, 600);
 
-
+// Set variables 
 const username = document.getElementById('username');
 const saveScore = document.getElementById('save-score');
 const finalScore = document.getElementById('final-score-total');
 const mostRecentScore = localStorage.getItem('recentScore');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
+// Max number of scores that can be displayed
 const maxHighScores = 5;
 
+// Set the final score that is displayed
 finalScore.innerText = mostRecentScore;
 
+// Disable the save score button if there is no value in the username
 username.addEventListener('keyup', () => {
     saveScore.disabled = !username.value;
   });
 
-let saveHighScore = e => {
+// Save the score to local storage
+function saveHighScore(e) {
     e.preventDefault();
 
+    // if (username == "" || username == " "){
+    //     console.log('No name inputted');
+    //     alert('You must input a name in order to save!');
+    //     return false;
+    // } else {
     const score = {
         score: mostRecentScore,
         name: username.value
     };
-
+    // Add the score to the highScores array, sort them in order and display the top 5
     highScores.push(score);
 
     highScores.sort((a, b) => {
         return b.score - a.score;
     });
 
-    highScores.splice(5);
+    highScores.splice(maxHighScores);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('https://daisygunn.github.io/around-the-world/additional-pages/high-scores.html');
+    window.location.assign('https://daisygunn.github.io/around-the-world/additional-pages/end-page.html');
 };
